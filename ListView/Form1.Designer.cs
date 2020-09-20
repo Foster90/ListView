6584e45bfb1aad5e7ce57fb93d0002569fd61856
Matthew Foster <Matthew.Foster@Civica.co.uk>
2020-09-20 11:00:13 +0100

    Project upload

--- ListView/Form1.Designer.cs
+++ ListView/Form1.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(218, 138);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(185, 131);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- ListView/Form1.cs
+++ ListView/Form1.cs
@@ -12,9 +12,60 @@ namespace ListView
 {
     public partial class Form1 : Form
     {
+        private ListViewColumnSorter lvwColumnSorter;
         public Form1()
         {
             InitializeComponent();
+           
+            lvwColumnSorter = new ListViewColumnSorter();
+            this.listView1.ListViewItemSorter = lvwColumnSorter;
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            listView1.View = View.Details;
+            listView1.Columns.Add("Column1Name");
+            listView1.Columns.Add("Column2Name");
+            listView1.Columns.Add("Column3Name");
+
+            listView1.Items.Add(new ListViewItem(new[] { "John  ", "1", "100" }));
+            listView1.Items.Add(new ListViewItem(new[] { "John ", "1", "100" }));
+            listView1.Items.Add(new ListViewItem(new[] { "Smith ", "2", "120" }));
+            listView1.Items.Add(new ListViewItem(new[] { "Cait ", "3", "97" }));
+            listView1.Items.Add(new ListViewItem(new[] { "Irene", "4", "100" }));
+
+            listView1.GridLines = true;
+
+
+        }
+
+        private void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+          
+
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                // Reverse the current sort direction for this column.
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                // Set the column number that is to be sorted; default to ascending.
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            // Perform the sort with these new sort options.
+            this.listView1.Sort();
         }
     }
 }
